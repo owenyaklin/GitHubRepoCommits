bc520e68b6d3b4f4557190fe0be71057c06c31ac
owenyaklin <owen.yaklin@gmail.com>
2022-02-03 22:42:39 -0600

    Font size increases to test scrolling.

--- components/ListItem.js
+++ components/ListItem.js
@@ -3,9 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function ListItem({ item }) {
     return (
         <View>
-            <Text>{item.author}</Text>
-            <Text>{item.hash}</Text>
-            <Text>{item.message}</Text>
+            <Text style={styles.viewText}>{item.author}</Text>
+            <Text style={styles.viewText}>{item.hash}</Text>
+            <Text style={styles.viewText}>{item.message}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    viewText: {
+        fontSize: 20
+    }
+});
