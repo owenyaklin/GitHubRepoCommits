b5de250b572bbe0cc1b8bd6e48155593f0ff0f56
owenyaklin <owen.yaklin@gmail.com>
2022-02-04 17:28:17 -0600

    Add pagination to GitHub commits fetch.

--- services/github.js
+++ services/github.js
@@ -1,10 +1,17 @@
 import { Octokit } from "@octokit/core";
 
-const getRepositoryCommits = async function (owner, repo) {
+const getRepositoryCommits = async function (owner, repo, pageSize = -1, pageNumber = -1) {
     const octokit = new Octokit();
     let resultArray = [];
+    let pageString = '';
+    if (pageSize !== -1) {
+        pageString = `?per_page=${pageSize}`;
+        if (pageNumber !== -1) {
+            pageString = `${pageString}&page=${pageNumber}`
+        }
+    }
     try {
-        const response = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+        const response = await octokit.request(`GET /repos/{owner}/{repo}/commits${pageString}`, {
             owner: owner,
             repo: repo
         });
--- services/github.test.js
+++ services/github.test.js
@@ -8,4 +8,15 @@ describe('Test Request', () => {
         let commits = await getRepositoryCommits(githubOwner, githubRepo);
         expect(Array.isArray(commits)).toBe(true);
     });
+    it('pagination', async () => {
+        let commits = await getRepositoryCommits(githubOwner, githubRepo);
+        if (commits.length > 1) {
+            let commits2 = await getRepositoryCommits(githubOwner, githubRepo, 1, 1);
+            expect(commits2.length).toBe(1);
+            expect(commits2[0].hash).toBe(commits[0].hash);
+            let commits3 = await getRepositoryCommits(githubOwner, githubRepo, 1, 2);
+            expect(commits3.length).toBe(1);
+            expect(commits3[0].hash).toBe(commits[1].hash);
+        }
+    });
 });
