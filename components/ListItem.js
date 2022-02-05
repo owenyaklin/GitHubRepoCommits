cf5a8f32c9b8b86ba1a52ea851afce582c0ab640
owenyaklin <owen.yaklin@gmail.com>
2022-02-04 21:03:02 -0600

    First attempt at adding style to List Items.

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,17 +1,35 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function ListItem({ item }) {
     return (
-        <View>
-            <Text style={styles.viewText}>{item.author}</Text>
-            <Text style={styles.viewText}>{item.hash}</Text>
-            <Text style={styles.viewText}>{item.message}</Text>
+        <View style={styles.listItemView}>
+            <Text style={styles.listItemHeaderText}>Author:</Text>
+            <Text style={styles.listItemText}>{item.author}</Text>
+            <Text style={styles.listItemHeaderText}>Hash:</Text>
+            <Text style={styles.listItemHashText}>{item.hash}</Text>
+            <Text style={styles.listItemHeaderText}>Message:</Text>
+            <Text style={styles.listItemText}>{item.message}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    viewText: {
+    listItemHashText: {
+        fontSize: 14
+    },
+    listItemHeaderText: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    listItemText: {
         fontSize: 20
+    },
+    listItemView: {
+        backgroundColor: '#e0e0e0',
+        borderColor: '#000000',
+        borderRadius: 5,
+        borderWidth: 1,
+        margin: 3
     }
 });
