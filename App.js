44b6884888bd989208eb507681aea80646289e04
owenyaklin <owen.yaklin@gmail.com>
2022-02-04 20:13:10 -0600

    Add onEndReached to FlatList to make commit fetch a paginated process.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
 import Constants from 'expo-constants';
 
 import ListItem from "./components/ListItem";
@@ -9,24 +9,56 @@ import getRepositoryCommits from './services/github';
 const githubOwner = process.env.GITHUB_OWNER;
 const githubRepo = process.env.GITHUB_REPO;
 
+const PAGE_SIZE = 10;
+
 export default function App() {
   const [commitsFetched, setCommitsFeteched] = useState(false);
   const [commits, setCommits] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [lastCommitReached, setLastCommitReached] = useState(false);
+  const [updateInProgress, setUpdateInProgress] = useState(false);
 
   if (!commitsFetched) {
-    getRepositoryCommits(githubOwner, githubRepo)
+    getRepositoryCommits(githubOwner, githubRepo, PAGE_SIZE, pageNumber)
       .then(function (githubData) {
         setCommits(githubData);
+        setUpdateInProgress(false);
+        if (githubData.length < PAGE_SIZE) {
+          setLastCommitReached(true);
+        }
+      })
+      .catch(function (fetchError) {
+        setUpdateInProgress(false);
       });
     setCommitsFeteched(true);
+    setUpdateInProgress(true);
   }
+
+  const loadMoreCommits = async info => {
+    if (!(updateInProgress || lastCommitReached)) {
+      let nextPage = pageNumber + 1;
+      getRepositoryCommits(githubOwner, githubRepo, PAGE_SIZE, nextPage)
+        .then(function (newData) {
+          setCommits(commits.concat(newData));
+          setUpdateInProgress(false);
+          if (newData.length < PAGE_SIZE) {
+            setLastCommitReached(true);
+          }
+        })
+        .catch(function (fetchError) {
+          setUpdateInProgress(false);
+        });;
+      setPageNumber(nextPage);
+      setUpdateInProgress(true);
+    }
+  };
 
   const renderItem = ({ item }) => (
     <ListItem item={item} />
   );
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View>
         <Text>Most recent commits for {githubOwner}/{githubRepo}</Text>
       </View>
@@ -34,8 +66,12 @@ export default function App() {
         data={commits}
         renderItem={renderItem}
         keyExtractor={item => item.hash}
+        onEndReachedThreshold={0.1}
+        onEndReached={info => {
+          loadMoreCommits(info);
+        }}
       />
-    </View>
+    </SafeAreaView >
   );
 }
 
